03f0e1d6af90d0904d08d95cb4949d421bbffb82
Vinay2009 <91785104+Vinay2009@users.noreply.github.com>
2022-03-29 16:22:06 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -29,12 +29,47 @@ function send() {
     charAt3 = word.charAt(length_minus_1);
     console.log(charAt3);
 
+    length_divide=Math.floor(word.length/3);
+    charAt4=word.charAt(length_divide);
+    console.log(charAt4);
+
+    length_minus=length_divide.length-1;
+    charAt5 = word.charAt(length_minus);
+    console.log(charAt5);
+
+    length_divide_1=Math.floor(length_minus.length/2);
+    charAt6=word.charAt(length_divide_1);
+    console.log(charAt6);
+
+    length_minus_2=length_minus.length-1;
+    charAt7 = word.charAt(length_minus_2);
+    console.log(charAt7);
+
+    length_divide_3=Math.floor(length_minus_2.length/2);
+    charAt8=word.charAt(length_divide_3);
+    console.log(charAt8);
+
+    length_minus_3=length_minus_2.length - 1;
+    charAt9 = word.charAt(length_minus_3);
+    console.log(charAt9);
+
+    length_divide_4=Math.floor(length_minus_3.length/2);
+    charAt10=word.charAt(length_divide_4);
+    console.log(charAt10);
+
     remove_charAt1=word.replace(charAt1, "_");
     remove_charAt2=remove_charAt1.replace(charAt2, "_");
     remove_charAt3=remove_charAt2.replace(charAt3, "_");
-    console.log(remove_charAt3);
+    remove_charAt4=remove_charAt3.replace(charAt4, "_");
+    remove_charAt5=remove_charAt4.replace(charAt5, "_");
+    remove_charAt6=remove_charAt5.replace(charAt6, "_");
+    remove_charAt7=remove_charAt6.replace(charAt7, "_");
+    remove_charAt8=remove_charAt7.replace(charAt8, "_");
+    remove_charAt9=remove_charAt8.replace(charAt9, "_");
+    remove_charAt10=remove_charAt9.replace(charAt10, "_");
+    console.log(remove_charAt10);
 
-    question_word= "<h4 id='word_display'> Q-" +remove_charAt3+"</h4>";
+    question_word= "<h4 id='word_display'> Q-" +remove_charAt10+"</h4>";
     input_box="<br>Answer:<input type='text' id='input_check_box'>";
     check_button= "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     row = question_word+input_box + check_button;
